9bf69818d90ad1a9eeac91d38693b4fd1715684e
SeanIsTethered <157828245+seanistethered@users.noreply.github.com>
2025-05-15 12:41:43 +0200

    Finished Settings page for now

--- Nyxian/UI/Settings/SettingsViewController.swift
+++ Nyxian/UI/Settings/SettingsViewController.swift
@@ -19,30 +19,102 @@ class SettingsViewController: UITableViewController {
         self.tableView.rowHeight = 44
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 4
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        switch section {
+        case 0:
+            return 2
+        case 1:
+            return 1
+        case 2:
+            return 1
+        case 3:
+            return 2
+        default:
+            return 0
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Compilers translate human readable source code into machine code, while linkers combine compiled code and libraries into a final executable file."
+        case 1:
+            return "An incremental build compiles only the parts of the code that have changed, reducing build times by avoiding a full rebuild of the entire project."
+        case 2:
+            return "Threading in compilation refers to the compiler's ability to perform tasks in parallel like parsing, code generation, and optimization across multiple CPU threads to speed up the build process."
+        default:
+            return nil
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "General"
+        case 3:
+            return "Special"
+        default:
+            return nil
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = PickerTableCell(
-            options: (indexPath.row == 0) ? ["LLVM"] : ["GNU"],
-            title: (indexPath.row == 0) ? "Compiler" : "Linker",
-            key: (indexPath.row == 0) ? "LDECompiler" : "LDELinker",
-            defaultValue: 0
-        )
+        let cell: UITableViewCell
+        
+        switch indexPath.section {
+        case 0:
+            cell = PickerTableCell(
+                options: (indexPath.row == 0) ? ["LLVM"] : ["GNU"],
+                title: (indexPath.row == 0) ? "Compiler" : "Linker",
+                key: (indexPath.row == 0) ? "LDECompiler" : "LDELinker",
+                defaultValue: 0
+            )
+            break
+        case 1:
+            cell = SwitchTableCell(title: "Incremental Build", key: "LDEIncrementalBuild", defaultValue: true)
+            break
+        case 2:
+            cell = SwitchTableCell(title: "Threaded Build", key: "LDEThreadedBuild", defaultValue: true)
+            break
+        case 3:
+            cell = ButtonTableCell(title: (indexPath.row == 0) ? "Import Certificate" : "Reset All")
+            (cell as! ButtonTableCell).button?.addAction(UIAction(handler: { _ in
+                if indexPath.row == 0 {
+                    let importPopup: CertificateImporter = CertificateImporter() {}
+                    let importSettings: UINavigationController = UINavigationController(rootViewController: importPopup)
+                    importSettings.modalPresentationStyle = .pageSheet
+                    self.present(importSettings, animated: true)
+                } else {
+                    let alert: UIAlertController = UIAlertController(
+                        title: "Warning",
+                        message: "All projects and preferences will be wiped! Are you sure you wanna proceed?",
+                        preferredStyle: .alert
+                    )
+                    
+                    alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+                    alert.addAction(UIAlertAction(title: "Proceed", style: .destructive) { _ in
+                        if let appDomain = Bundle.main.bundleIdentifier {
+                            UserDefaults.standard.removePersistentDomain(forName: appDomain)
+                            UserDefaults.standard.synchronize()
+                        }
+                        
+                        Bootstrap.shared.bootstrapVersion = 0
+                        Bootstrap.shared.clearPath(path: "/")
+                        restartProcess()
+                    })
+                    
+                    self.present(alert, animated: true)
+                }
+            }), for: .touchUpInside)
+            break
+        default:
+            cell = UITableViewCell()
+        }
         
         return cell
-    }
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Toolchain Libraries"
-    }
-
-    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-        return "Compilers translate human readable source code into machine code, while linkers combine compiled code and libraries into a final executable file."
     }
 }
---
+++ Nyxian/UI/TableCells/SwitchCell.swift
@@ -0,0 +1,80 @@
+//
+//  SwitchCell.swift
+//  Nyxian
+//
+//  Created by FridaDEV on 15.05.25.
+//
+
+import Foundation
+import UIKit
+
+class SwitchTableCell: UITableViewCell {
+    let title: String
+    
+    let key: String
+    let defaultValue: Bool
+    var value: Bool {
+        get {
+            if UserDefaults.standard.object(forKey: self.key) == nil {
+                UserDefaults.standard.set(self.defaultValue, forKey: self.key)
+            }
+            
+            return UserDefaults.standard.bool(forKey: self.key)
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: self.key)
+        }
+    }
+    
+    init(
+        title: String,
+        key: String,
+        defaultValue: Bool
+    ) {
+        self.title = title
+        self.key = key
+        self.defaultValue = defaultValue
+        super.init(style: .default, reuseIdentifier: nil)
+        
+        _ = self.value
+        
+        self.setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        // disable selection
+        self.selectionStyle = .none
+        
+        // First create the label
+        let label: UILabel = UILabel()
+        label.text = self.title
+        label.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(label)
+        
+        // now the option button
+        let toggle: UISwitch = UISwitch()
+        toggle.setOn(self.value, animated: false)
+        toggle.translatesAutoresizingMaskIntoConstraints = false
+        toggle.addTarget(self, action: #selector(toggleValueChanged), for: .valueChanged)
+        self.contentView.addSubview(toggle)
+        
+        // Now fix its constraints
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            label.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
+            label.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 16),
+            
+            toggle.leadingAnchor.constraint(equalTo: label.trailingAnchor, constant: 16),
+            toggle.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -16),
+            toggle.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor)
+        ])
+    }
+    
+    @objc private func toggleValueChanged(_ sender: UISwitch) {
+        self.value = sender.isOn
+    }
+}
