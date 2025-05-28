b80a9d727cdc43020018b15b00557f38d24c622d
SeanIsTethered <157828245+seanistethered@users.noreply.github.com>
2025-05-28 17:41:16 +0200

    LDEThemer Refinement

--- Nyxian/UI/CodeEditor/CodeEditor+Theme.swift
+++ Nyxian/UI/CodeEditor/CodeEditor+Theme.swift
@@ -60,6 +60,27 @@ func gibDynamicColor(light: UIColor, dark: UIColor) -> UIColor {
     })
 }
 
+extension UIColor {
+    convenience init(light: (CGFloat, CGFloat, CGFloat), dark: (CGFloat, CGFloat, CGFloat), alpha: Double = 1.0) {
+        let light: UIColor = neoRGB(light.0, light.1, light.2).withAlphaComponent(alpha)
+        let dark: UIColor = neoRGB(dark.0, dark.1, dark.2).withAlphaComponent(alpha)
+        
+        self.init(dynamicProvider: { traits in
+            switch traits.userInterfaceStyle {
+            case .light, .unspecified:
+                return light
+                
+            case .dark:
+                return dark
+                
+            @unknown default:
+                assertionFailure("Unknown userInterfaceStyle: \(traits.userInterfaceStyle)")
+                return light
+            }
+        })
+    }
+}
+
 class LindDEThemer: Theme {
     var fontSize: CGFloat = CGFloat(UserDefaults.standard.double(forKey: "CEFontSize"))
     
@@ -71,134 +92,38 @@ class LindDEThemer: Theme {
         return UIFont.monospacedSystemFont(ofSize: fontSize * 0.85, weight: .regular)
     }
     
-    var textColor: UIColor
-    var backgroundColor: UIColor
+    let textColor: UIColor = UIColor(light: (30, 30, 30), dark: (232, 242, 255))
+    let backgroundColor: UIColor = UIColor(light: (250, 250, 250), dark: (30, 30, 30))
     
-    var gutterBackgroundColor: UIColor
-    var gutterHairlineColor: UIColor
+    let gutterBackgroundColor: UIColor = UIColor(light: (240, 240, 240), dark: (25, 25, 25))
+    let gutterHairlineColor: UIColor = UIColor(light: (220, 220, 220), dark: (50, 50, 50))
     
-    var lineNumberColor: UIColor
+    let lineNumberColor: UIColor = UIColor(light: (180, 180, 180), dark: (90, 90, 90))
     
-    var selectedLineBackgroundColor: UIColor
-    var selectedLinesLineNumberColor: UIColor
-    var selectedLinesGutterBackgroundColor: UIColor
+    let selectedLineBackgroundColor: UIColor = UIColor(light: (220, 230, 250), dark: (40, 44, 52), alpha: 0.8)
+    let selectedLinesLineNumberColor: UIColor = UIColor(light: (80, 120, 180), dark: (130, 180, 255))
+    let selectedLinesGutterBackgroundColor: UIColor = UIColor(light: (235, 235, 240), dark: (20, 20, 25))
     
-    var invisibleCharactersColor: UIColor
-    
-    var pageGuideHairlineColor: UIColor
-    var pageGuideBackgroundColor: UIColor
-    
-    var markedTextBackgroundColor: UIColor
-    
-    let colorKeyword: UIColor
-    let colorComment: UIColor
-    let colorString: UIColor
-    let colorNumber: UIColor
-    let colorRegex: UIColor
-    let colorFunction: UIColor
-    let colorOperator: UIColor
-    let colorProperty: UIColor
-    let colorPunctuation: UIColor
-    let colorDirective: UIColor
-    let colorType: UIColor
-    let colorConstantBuiltin: UIColor
-    
-    init() {
-        backgroundColor = gibDynamicColor(
-            light:  neoRGB(250, 250, 250),
-            dark:   neoRGB(30, 30, 30)
-        )
-        textColor = gibDynamicColor(
-            light:  neoRGB(30, 30, 30),
-            dark:   neoRGB(232, 242, 255)
-        )
-        gutterBackgroundColor = gibDynamicColor(
-            light:  neoRGB(240, 240, 240),
-            dark:   neoRGB(25, 25, 25)
-        )
-        gutterHairlineColor = gibDynamicColor(
-            light:  neoRGB(220, 220, 220),
-            dark:   neoRGB(50, 50, 50)
-        )
-        lineNumberColor = gibDynamicColor(
-            light:  neoRGB(180, 180, 180),
-            dark:   neoRGB(90, 90, 90)
-        )
-        selectedLineBackgroundColor = gibDynamicColor(
-            light:  neoRGB(220, 230, 250).withAlphaComponent(0.8),
-            dark:   neoRGB(40, 44, 52).withAlphaComponent(0.8)
-        )
-        selectedLinesLineNumberColor = gibDynamicColor(
-            light:  neoRGB(80, 120, 180),
-            dark:   neoRGB(130, 180, 255)
-        )
-        selectedLinesGutterBackgroundColor = gibDynamicColor(
-            light:  neoRGB(235, 235, 240),
-            dark:   neoRGB(20, 20, 25)
-        )
-        pageGuideHairlineColor = gibDynamicColor(
-            light:  neoRGB(220, 220, 230),
-            dark:   neoRGB(45, 45, 50)
-        )
-        pageGuideBackgroundColor = gibDynamicColor(
-            light:  neoRGB(245, 245, 250),
-            dark:   neoRGB(30, 30, 35)
-        )
-        markedTextBackgroundColor = gibDynamicColor(
-            light:  neoRGB(230, 240, 250),
-            dark:   neoRGB(60, 60, 70)
-        )
-        colorKeyword = gibDynamicColor(
-            light:  neoRGB(0, 0, 192),
-            dark:   neoRGB(86, 156, 214)
-        )
-        colorComment = gibDynamicColor(
-            light:  neoRGB(0, 128, 0),
-            dark:   neoRGB(106, 153, 85)
-        )
-        colorString = gibDynamicColor(
-            light:  neoRGB(163, 21, 21),
-            dark:   neoRGB(206, 145, 120)
-        )
-        colorNumber = gibDynamicColor(
-            light:  neoRGB(128, 0, 128),
-            dark:   neoRGB(181, 206, 168)
-        )
-        colorRegex = gibDynamicColor(
-            light:  neoRGB(255, 140, 0),
-            dark:   neoRGB(255, 198, 109)
-        )
-        colorFunction = gibDynamicColor(
-            light:  neoRGB(43, 145, 175),
-            dark:   neoRGB(220, 220, 170)
-        )
-        colorOperator = gibDynamicColor(
-            light:  neoRGB(0, 0, 0),
-            dark:   neoRGB(212, 212, 212)
-        )
-        colorProperty = gibDynamicColor(
-            light:  neoRGB(0, 0, 128),
-            dark:   neoRGB(156, 220, 254)
-        )
-        colorPunctuation = gibDynamicColor(
-            light:  neoRGB(50, 50, 50),
-            dark:   neoRGB(200, 200, 200)
-        )
-        colorDirective = gibDynamicColor(
-            light:  neoRGB(60, 60, 60),
-            dark:   neoRGB(255, 165, 0)
-        )
-        colorType = gibDynamicColor(
-            light:  neoRGB(43, 43, 150),
-            dark:   neoRGB(78, 201, 176)
-        )
-        colorConstantBuiltin = gibDynamicColor(
-            light:  neoRGB(75, 0, 130),
-            dark:   neoRGB(0, 255, 255)
-        )
-        
-        invisibleCharactersColor = textColor.withAlphaComponent(0.25)
+    var invisibleCharactersColor: UIColor {
+        return textColor.withAlphaComponent(0.25)
     }
+    
+    let pageGuideHairlineColor: UIColor = UIColor(light: (220, 220, 230), dark: (45, 45, 50))
+    let pageGuideBackgroundColor: UIColor = UIColor(light: (245, 245, 250), dark: (30, 30, 35))
+    
+    var markedTextBackgroundColor: UIColor = UIColor(light: (230, 240, 250), dark: (60, 60, 70))
+    let colorKeyword: UIColor = UIColor(light: (0, 0, 192), dark: (86, 156, 214))
+    let colorComment: UIColor = UIColor(light: (0, 128, 0), dark: (106, 153, 85))
+    let colorString: UIColor = UIColor(light: (163, 21, 21), dark: (206, 145, 120))
+    let colorNumber: UIColor = UIColor(light: (128, 0, 128), dark: (181, 206, 168))
+    let colorRegex: UIColor = UIColor(light: (255, 140, 0), dark: (255, 198, 109))
+    let colorFunction: UIColor = UIColor(light: (43, 145, 175), dark: (220, 220, 170))
+    let colorOperator: UIColor = UIColor(light: (0, 0, 0), dark: (212, 212, 212))
+    let colorProperty: UIColor = UIColor(light: (0, 0, 128), dark: (156, 220, 254))
+    let colorPunctuation: UIColor = UIColor(light: (50, 50, 50), dark: (200, 200, 200))
+    let colorDirective: UIColor = UIColor(light: (60, 60, 60), dark: (255, 165, 0))
+    let colorType: UIColor = UIColor(light: (43, 43, 150), dark: (78, 201, 176))
+    let colorConstantBuiltin: UIColor = UIColor(light: (75, 0, 130), dark: (0, 255, 255))
     
     func textColor(for highlightName: String) -> UIColor? {
         guard let highlightName = HighlightName(highlightName) else {
