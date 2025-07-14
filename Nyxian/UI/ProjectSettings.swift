71ac7e8100a69d814c8e8ef42c1876cd2a19e6c0
cr4zyengineer <fridakitten@pm.me>
2025-07-14 13:46:08 +0200

    Extended and improved project settings

--- Nyxian/UI/ProjectSettings.swift
+++ Nyxian/UI/ProjectSettings.swift
@@ -11,16 +11,24 @@ import UIKit
 class ProjectSettingsViewController: UITableViewController {
     let project: AppProject
     
-    let vtkey: [(String, String, String)]
+    let vtkey: [[(String, String, String)]]
     
     init(style: UITableView.Style,
          project: AppProject) {
         self.project = project
         self.vtkey = [
-            (self.project.projectConfig.executable,"Executable","LDEExecutable"),
-            (self.project.projectConfig.displayname,"Display Name","LDEDisplayName"),
-            (self.project.projectConfig.bundleid,"BundleID","LDEBundleIdentifier")
+            [
+                (self.project.projectConfig.executable,"Executable","LDEExecutable"),
+                (self.project.projectConfig.displayname,"Display Name","LDEDisplayName"),
+                (self.project.projectConfig.bundleid,"BundleID","LDEBundleIdentifier"),
+                (self.project.projectConfig.version,"Version","LDEBundleVersion"),
+                (self.project.projectConfig.shortVersion,"Short Version","LDEBundleShortVersion")
+            ],
+            [
+                (self.project.projectConfig.minimum_version,"Minimum Deployments","LDEMinimumVersion")
+            ]
         ]
+
         super.init(style: style)
     }
     
@@ -35,13 +43,28 @@ class ProjectSettingsViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.vtkey.count
+        return self.vtkey[section].count
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Info"
+        case 1:
+            return "Build"
+        default:
+            return nil
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: TextFieldTableCellHandler = TextFieldTableCellHandler(title: self.vtkey[indexPath.row].1, value: self.vtkey[indexPath.row].0)
+        let cell: TextFieldTableCellHandler = TextFieldTableCellHandler(title: self.vtkey[indexPath.section][indexPath.row].1, value: self.vtkey[indexPath.section][indexPath.row].0)
         cell.writeHandler = { value in
-            self.project.projectConfig.writeKey(key: self.vtkey[indexPath.row].2, value: value)
+            self.project.projectConfig.writeKey(key: self.vtkey[indexPath.section][indexPath.row].2, value: value)
             self.project.projectTableCell.reload()
         }
         
