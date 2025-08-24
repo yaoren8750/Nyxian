48bbacc2a6ed49a449da292635707dc6d41365e7
cr4zyengineer <fridakitten@pm.me>
2025-08-25 01:20:56 +0200

    Improved FileList structures

--- Nyxian/UI/FileList/FileList+Structure.swift
+++ Nyxian/UI/FileList/FileList+Structure.swift
@@ -35,21 +35,15 @@ struct FileListEntry: Identifiable {
     
     static func getEntry(ofPath path: String) -> FileListEntry {
         var entry = FileListEntry(name: "N/A", path: path, isLink: false, type: .file)
-        var path = path
         do {
             var isDirectory: ObjCBool = false
-            let exists = FileManager.default.fileExists(atPath: path, isDirectory: &isDirectory)
-            if exists {
-                let attributes = try FileManager.default.attributesOfItem(atPath: path)
-                if let fileType = attributes[.type] as? FileAttributeType {
-                    let isLink: Bool = (fileType == .typeSymbolicLink) ? true : false
-                    if isLink { path = try FileManager.default.destinationOfSymbolicLink(atPath: path) }
-                    
-                    entry = FileListEntry(name: URL(fileURLWithPath: path).lastPathComponent,
-                                                 path: path,
-                                                 isLink: isLink,
-                                                 type: isDirectory.boolValue ? .dir : .file)
-                }
+            if FileManager.default.fileExists(atPath: path, isDirectory: &isDirectory),
+               let fileType = try FileManager.default.attributesOfItem(atPath: path)[.type] as? FileAttributeType {
+                let isLink: Bool = (fileType == .typeSymbolicLink)
+                entry = FileListEntry(name: URL(fileURLWithPath: path).lastPathComponent,
+                                      path: isLink ? try FileManager.default.destinationOfSymbolicLink(atPath: path) : path,
+                                      isLink: isLink,
+                                      type: isDirectory.boolValue ? .dir : .file)
             }
         } catch {
             print(error.localizedDescription)
@@ -59,43 +53,23 @@ struct FileListEntry: Identifiable {
     }
     
     static func getEntries(ofPath path: String) -> [FileListEntry] {
-        var entries: [FileListEntry] = []
-        
+        var entries: [FileListEntry]? = nil
         do {
             let rawEntries: [String] = try FileManager.default.contentsOfDirectory(atPath: path)
-            for rawEntry in rawEntries { entries.append(FileListEntry.getEntry(ofPath: "\(path)/\(rawEntry)")) }
+            entries = rawEntries.map { FileListEntry.getEntry(ofPath: "\(path)/\($0)") }
+            entries!.sort {
+                $0.type == $1.type
+                    ? $0.name.localizedCaseInsensitiveCompare($1.name) == .orderedAscending
+                    : $0.type == .dir
+            }
         } catch {
             print(error)
         }
-        
-        entries.sort {
-            if $0.type == $1.type {
-                return $0.name.localizedCaseInsensitiveCompare($1.name) == .orderedAscending
-            }
-            return $0.type == .dir
-        }
-        
-        return entries
+        return entries ?? []
     }
     
     static func getEntries(ofPath path: String, perEntryHandler: (FileListEntry) -> Void) {
-        var entries: [FileListEntry] = []
-        
-        do {
-            let rawEntries: [String] = try FileManager.default.contentsOfDirectory(atPath: path)
-            for rawEntry in rawEntries { entries.append(FileListEntry.getEntry(ofPath: "\(path)/\(rawEntry)")) }
-        } catch {
-            print(error)
-        }
-        
-        entries.sort {
-            if $0.type == $1.type {
-                return $0.name.localizedCaseInsensitiveCompare($1.name) == .orderedAscending
-            }
-            return $0.type == .dir
-        }
-        
-        for item in entries {
+        for item in FileListEntry.getEntries(ofPath: path) {
             perEntryHandler(item)
         }
     }
