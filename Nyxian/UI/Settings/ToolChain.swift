94df330d4c83bdd22af38aac5a23d88cb5b326d2
cr4zyengineer <fridakitten@pm.me>
2025-09-02 17:09:29 +0200

    Removing deprecated toolchain settings

--- Nyxian/UI/Settings/ToolChain.swift
+++ Nyxian/UI/Settings/ToolChain.swift
@@ -31,7 +31,7 @@ class ToolChainController: UIThemedTableViewController {
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 4
+        return 2
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -40,8 +40,6 @@ class ToolChainController: UIThemedTableViewController {
             return 1
         case 1:
             return 1
-        case 2:
-            return 2
         default:
             return 0
         }
@@ -53,8 +51,6 @@ class ToolChainController: UIThemedTableViewController {
             return "An incremental build compiles only the parts of the code that have changed, reducing build times by avoiding a full rebuild of the entire project."
         case 1:
             return "Threading in compilation refers to the compiler's ability to perform tasks in parallel like parsing, code generation, and optimization across multiple CPU threads to speed up the build process."
-        case 2:
-            return "The functionality makes sense when you want perfect memory management, this basically relaunches Nyxian in a smart way while preserving the UI state and restoring it very fast on reopening, this is stock on success although there is a trick that allows us to programatically relaunch the app, this might work on your device."
         default:
             return nil
         }
@@ -64,8 +60,6 @@ class ToolChainController: UIThemedTableViewController {
         switch section {
         case 0:
             return "Features"
-        case 2:
-            return "Experimental"
         default:
             return nil
         }
@@ -82,12 +76,6 @@ class ToolChainController: UIThemedTableViewController {
             let optimCpuCount: Int = (Int)(LDEThreadControl.getOptimalThreadCount())
             cell = StepperTableCell(title: "Use Threads", key: "cputhreads", defaultValue: optimCpuCount, minValue: 1, maxValue: optimCpuCount)
             break
-        case 2:
-            if indexPath.row == 0 {
-                cell = SwitchTableCell(title: "Restart App (On Failure)", key: "LDEReopen", defaultValue: false)
-            } else {
-                cell = SwitchTableCell(title: "Restart App (On Succeed)", key: "LDEReopenSucceed", defaultValue: true)
-            }
         default:
             cell = UITableViewCell()
         }
