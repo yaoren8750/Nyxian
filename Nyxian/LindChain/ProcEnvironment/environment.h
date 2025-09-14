f4745ebd1f319cf9f1037506ab0941fc2ee73824
cr4zyengineer <fridakitten@pm.me>
2025-09-14 21:36:57 +0200

    More documentation

--- Nyxian/LindChain/ProcEnvironment/environment.h
+++ Nyxian/LindChain/ProcEnvironment/environment.h
@@ -23,12 +23,19 @@
 #import <Foundation/Foundation.h>
 #import <LindChain/ProcEnvironment/Server/ServerProtocol.h>
 
+/// Boolean value that indicates if the target environment runs at is the host or the guest environment at runtime
 extern BOOL environmentIsHost;
 
+/// Connects client to host environment via its preshipped endpoint
 void environment_client_connect_to_host(NSXPCListenerEndpoint *endpoint);
+
+/// Hands off clients standard file descriptors to host environment so output gets redirected to host environment
 void environment_client_handoff_standard_file_descriptors(void);
+
+/// Attaches debugger to the guest environment it self, its a self debugger
 void environment_client_attach_debugger(void);
 
+/// Initilizes the environment, the boolean argument the symbol takes indicates if its the host or the client environment
 void environment_init(BOOL host);
 
 #endif /* PROCENVIRONMENT_ENVIRONMENT_H */
